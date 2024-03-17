776087090e0924a7914a2da5d75d5dca0a11b55b
KateCherednichok <chekavic@gmail.com>
2024-03-17 22:17:15 +0200

    tests for input from file

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -15,4 +15,20 @@ class TestInput(unittest.TestCase):
 
     def test_input_from_file_build_in_empty_file(self):
         result_of_function = input_from_file_build_in('../../data/test2.txt')
-        self.assertEquals(result_of_function, '')
+        self.assertTrue(len(result_of_function) == 0)
+
+
+    def test_input_from_file_by_pandas_file_not_exist(self):
+        with self.assertRaises(FileNotFoundError):
+            input_from_file_by_pandas('../../data/error.csv')
+
+
+    def test_input_from_file_by_pandas_file_empty_file(self):
+        with self.assertRaises(pandas.errors.EmptyDataError):
+            input_from_file_by_pandas('../../data/emptyTest.csv')
+
+
+    def test_input_from_file_by_pandas_file_exist(self):
+        result_of_function = input_from_file_by_pandas('../../data/test1.txt')
+        self.assertIsInstance(result_of_function, str)
+        self.assertTrue(len(result_of_function) > 0)
