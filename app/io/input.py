eb79e93443deb3ab11605776e515d705e41e7024
KateCherednichok <chekavic@gmail.com>
2024-03-17 09:46:36 +0200

    requirements.txt

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def input_from_console():
+    pass
+
+
+def input_from_file_build_in(file_name):
+    pass
+
+
+def input_from_file_by_pandas(file_name):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def output_in_console():
+    pass
+
+
+def output_in_file_build_in(file_name):
+    pass
